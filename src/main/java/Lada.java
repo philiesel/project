f63307fea5eb35d03eed4ab53790fe97fb2d79d7
philiesel <philiesel@yandex.ru>
2025-09-17 00:07:11 +0300

    Добавил альтернативные решения в методы в классе Main

--- src/main/java/Lada.java
+++ src/main/java/Lada.java
@@ -1,6 +1,17 @@
-public class Lada extends Car{
-    public Lada(String name, String model, int yearMake, String transmission, String color, String motor, int door, int carSeat) {
+public class Lada extends Car {
+    private int countMirror;
+
+    public Lada(String name, String model, int yearMake, String transmission, String color, String motor, int door, int carSeat, int countMirror) {
         super(name, model, yearMake, transmission, color, motor, door, carSeat);
+        this.countMirror = countMirror;
+    }
+
+    public int getCountMirror() {
+        return countMirror;
+    }
+
+    public void setCountMirror(int countMirror) {
+        this.countMirror = countMirror;
     }
 
     @Override
@@ -13,10 +24,11 @@ public class Lada extends Car{
         System.out.println("Lada остановилась");
     }
 
-    public void turnOnMusic(){
+    public void turnOnMusic() {
         System.out.println("Музыка включилась");
     }
-    public void turnOfMusic(){
+
+    public void turnOfMusic() {
         System.out.println("Музыка выключилась");
     }
 }
