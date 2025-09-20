0abe2254750b4c98e6981d6ae7c3094f17e65725
philiesel <philiesel@yandex.ru>
2025-09-20 12:32:02 +0300

    Revert "Добавил альтернативные решения в методы в классе Main"
    This reverts commit f63307fea5eb35d03eed4ab53790fe97fb2d79d7.

--- src/main/java/Lada.java
+++ src/main/java/Lada.java
@@ -1,17 +1,6 @@
-public class Lada extends Car {
-    private int countMirror;
-
-    public Lada(String name, String model, int yearMake, String transmission, String color, String motor, int door, int carSeat, int countMirror) {
+public class Lada extends Car{
+    public Lada(String name, String model, int yearMake, String transmission, String color, String motor, int door, int carSeat) {
         super(name, model, yearMake, transmission, color, motor, door, carSeat);
-        this.countMirror = countMirror;
-    }
-
-    public int getCountMirror() {
-        return countMirror;
-    }
-
-    public void setCountMirror(int countMirror) {
-        this.countMirror = countMirror;
     }
 
     @Override
@@ -24,11 +13,10 @@ public class Lada extends Car {
         System.out.println("Lada остановилась");
     }
 
-    public void turnOnMusic() {
+    public void turnOnMusic(){
         System.out.println("Музыка включилась");
     }
-
-    public void turnOfMusic() {
+    public void turnOfMusic(){
         System.out.println("Музыка выключилась");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     private List<Car> carCollection = new ArrayList<>();
 
     public Main() {
         this.carCollection.add(new Aurus("Aurus", "V234", 2004, "Механика", "Зеленый цвет", "Бензиновый", 4, 4));
-        this.carCollection.add(new Aurus("Aurus", "V674", 2010, "Автомат", " Зеленый цвет ", "Дизельный", 2, 2));
+        this.carCollection.add(new Aurus("Aurus", "V674", 2010, "Автомат", "Зеленый цвет", "Дизельный", 2, 2));
         this.carCollection.add(new Suzuki("Suzuki", "N324", 2006, "Механика", "Синий цвет", "Бензиновый", 2, 2));
         this.carCollection.add(new Aurus("Aurus", "G342", 2014, "Механика", "Желтый цвет", "Бензиновый", 4, 4));
         this.carCollection.add(new Changan("Changan", "B654", 2001, "Автомат", "Красный цвет", "Дизельный", 4, 4));
         this.carCollection.add(new Suzuki("Suzuki", "X127", 2000, "Механика", "Белый цвет", "Дизельный", 4, 4));
-        this.carCollection.add(new Lada("Lada", "K897", 2001, "Автомат", "Черный цвет", "Бензиновый", 2, 2, 3));
-        this.carCollection.add(new Lada("Lada", "F789", 2002, "Автомат", "Розовый цвет", "Бензиновый", 4, 4, 4));
+        this.carCollection.add(new Lada("Lada", "K897", 2001, "Автомат", "Черный цвет", "Бензиновый", 2, 2));
+        this.carCollection.add(new Lada("Lada", "F789", 2002, "Автомат", "Розовый цвет", "Бензиновый", 4, 4));
         this.carCollection.add(new Suzuki("Suzuki", "R466", 2005, "Механика", "Оранжевый цвет", "Бензиновый", 2, 2));
         this.carCollection.add(new Toyota("Toyota", "L976", 2003, "Механика", "Коричневый цвет", "Дизельный", 2, 2));
         this.carCollection.add(new Toyota("Toyota", "V764", 2010, "Механика", "Зеленый цвет", "Бензиновый", 4, 4));
@@ -34,29 +33,17 @@ public class Main {
 
     public void changeColorCar() {
         for (Car car : carCollection) {
-            if (car.getColor().trim().equalsIgnoreCase("Зеленый цвет")) {
+            if (car.getColor().equalsIgnoreCase("Зеленый цвет")) {
                 car.setColor("Красный цвет");
             }
         }
-//        Можно реализовать через stream
-//        carCollection.stream()
-//                .filter(car -> car.getColor().equalsIgnoreCase("Зеленый цвет"))
-//                .forEach(car -> car.setColor("Красный цвет"));
     }
-
-    public void changeMotor() {
+    public void changeMotor(){
         for (Car car : carCollection) {
-            if (car.getMotor().trim().equalsIgnoreCase("Дизельный")) {
+            if(car.getMotor().equalsIgnoreCase("Дизельный")) {
                 car.setMotor("Электрический");
             }
         }
-//        Можно реализовать через forEach
-//        carCollection.forEach(car -> {
-//            if (car.getMotor().equalsIgnoreCase("Дизельный")) {
-//                car.setMotor("Электрический");
-//            }
-//        });
-
     }
 
     //it's Main origin
