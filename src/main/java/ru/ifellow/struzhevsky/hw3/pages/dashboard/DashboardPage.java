86d4c69813faf0de31edb475a8159201e9824948
philiesel <philiesel@yandex.ru>
2025-09-21 23:49:47 +0300

    Перенес метод перехода на Test проект.

--- src/main/java/ru/ifellow/struzhevsky/hw3/pages/dashboard/DashboardPage.java
+++ src/main/java/ru/ifellow/struzhevsky/hw3/pages/dashboard/DashboardPage.java
@@ -3,16 +3,24 @@ package ru.ifellow.struzhevsky.hw3.pages.dashboard;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import ru.ifellow.struzhevsky.hw3.pages.profile.ProfilePage;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.page;
 
 public class DashboardPage {
     private final SelenideElement buttonProfileLocator = $x("//a[@id='header-details-user-fullname']");
     private final SelenideElement linkProfileLocator = $x("//a[@id='view_profile']");
+    private SelenideElement menuProject = $x("//a[@id='browse_link']");
+    private SelenideElement linkProjectTest = $x("//a[@id='admin_main_proj_link_lnk']");
 
     public ProfilePage goToProfile() {
         buttonProfileLocator.click();
         linkProfileLocator.shouldBe(Condition.visible).click();
         return page(ProfilePage.class);
+    }
+
+    public void goToProjectTest() {
+        menuProject.shouldBe(Condition.visible).click();
+        linkProjectTest.click();
     }
 }
--- src/test/java/ru/ifellow/struzhevsky/hw3/LinkToProjectTest.java
+++ src/test/java/ru/ifellow/struzhevsky/hw3/LinkToProjectTest.java
@@ -2,19 +2,22 @@ package ru.ifellow.struzhevsky.hw3;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import ru.ifellow.struzhevsky.hw3.pages.dashboard.DashboardPage;
 import ru.ifellow.struzhevsky.hw3.pages.login.LoginPage;
 import ru.ifellow.struzhevsky.hw3.pages.project.ProjectPage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LinkToProjectTest extends BaseTest {
     private final LoginPage loginPage = new LoginPage();
+    private final DashboardPage dashboardPage = new DashboardPage();
     private final ProjectPage projectPage = new ProjectPage();
 
     @ParameterizedTest
     @ValueSource(strings = {"TEST"})
     public void goToProjectTest(String metaValue) {
         loginPage.auth(BaseTest.username, BaseTest.password);
-        projectPage.goToProjectTest();
+        dashboardPage.goToProjectTest();
+        projectPage.clickButtAvatar();
         assertEquals(metaValue, projectPage.getMetaValueProject());
     }
 }
