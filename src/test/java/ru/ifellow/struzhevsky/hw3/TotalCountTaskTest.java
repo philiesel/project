00a3c40c001ac6038c7a2fe1ba7e3c382acaf332
philiesel <philiesel@yandex.ru>
2025-09-22 00:23:35 +0300

    Добавил тест проверки общего количества заведенных задач в проекте.

---
+++ src/test/java/ru/ifellow/struzhevsky/hw3/TotalCountTaskTest.java
@@ -0,0 +1,33 @@
+package ru.ifellow.struzhevsky.hw3;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import ru.ifellow.struzhevsky.hw3.pages.dashboard.DashboardPage;
+import ru.ifellow.struzhevsky.hw3.pages.login.LoginPage;
+import ru.ifellow.struzhevsky.hw3.pages.project.ProjectPage;
+import ru.ifellow.struzhevsky.hw3.pages.task.FormTask;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TotalCountTaskTest extends BaseTest{
+    private LoginPage loginPage = new LoginPage();
+    private ProjectPage projectPage = new ProjectPage();
+    private DashboardPage dashboardPage = new DashboardPage();
+    private FormTask formTask = new FormTask();
+    private String descriptionTask = "Test Task";
+    private String typeBugIssue = "Задача";
+
+    @Test
+    public void checkCountTask() {
+        loginPage.auth(BaseTest.username, BaseTest.password);
+        dashboardPage.goToProjectTest();
+        projectPage.clickMenuTask();
+        int countTask = projectPage.parsCountTaskOnProject();
+        projectPage.clickButtNewTask();
+        formTask.selectTypeBug(typeBugIssue);
+        formTask.fillFieldTopicTask(descriptionTask);
+        formTask.clickButtCreateNewIssue();
+        Selenide.refresh();
+        int updateCountTask = projectPage.parsCountTaskOnProject();
+        assertEquals(countTask+1, updateCountTask);
+    }
+}
