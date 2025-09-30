3d7b2e21be019488a1ffd95f0e978d767e481ac1
philiesel <philiesel@yandex.ru>
2025-09-30 16:33:41 +0300

    Добавил описание в локаторы класса Проект.

--- src/main/java/ru/ifellow/struzhevsky/hw3/pages/ProjectPage.java
+++ src/main/java/ru/ifellow/struzhevsky/hw3/pages/ProjectPage.java
@@ -6,12 +6,12 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ProjectPage {
-    private final SelenideElement countTaskOnProject = $x("//div[@class='showing']/span");
-    private final SelenideElement linkAllTask = $x("//a[text()='Посмотреть все задачи и фильтры']");
-    private final SelenideElement buttonCreateTask = $x("//a[@id='create_link']");  // заменить
-    private final SelenideElement metaValueProject = $x("//dd[@class='project-meta-value' and text()='TEST']");
-    private final SelenideElement buttonTask = $x("//a[.//span[@class='aui-nav-item-label' and @title='Задачи']]");
-    private final SelenideElement buttonAvatar = $x("//a[@title='Test' and contains(@class, 'jira-project-avatar')]");
+    private final SelenideElement countTaskOnProject = $x("//div[@class='showing']/span").as("Надпись кол-во задач");
+    private final SelenideElement linkAllTask = $x("//a[text()='Посмотреть все задачи и фильтры']").as("Все задачи и фильтры");
+    private final SelenideElement buttonCreateTask = $x("//a[@id='create_link']").as("Создать новую задачу");  // заменить
+    private final SelenideElement metaValueProject = $x("//dd[@class='project-meta-value' and text()='TEST']").as("Тег проекта");
+    private final SelenideElement buttonTask = $x("//a[.//span[@class='aui-nav-item-label' and @title='Задачи']]").as("Кнопка \"Задачи\"");
+    private final SelenideElement buttonAvatar = $x("//a[@title='Test' and contains(@class, 'jira-project-avatar')]").as("Аватарка");
 
     public int parsCountTaskOnProject() {
         String taskText = countTaskOnProject.shouldBe(Condition.visible).text();
