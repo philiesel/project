1b79b5748fa324c02ce5aa072893325ce123b653
philiesel <philiesel@yandex.ru>
2025-10-26 17:06:59 +0300

    Обновил конфигурационный класс.

--- src/main/java/ru/ifellow/struzhevsky/hw3/utils/ServiceData.java
+++ src/main/java/ru/ifellow/struzhevsky/hw3/utils/ServiceData.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 public class ServiceData {
     private static final String CONFIG_PATH = "src/test/resources/config.properties";
     private static final String TEST_DATA_PATH = "src/test/resources/testData.properties";
+    private static final String ALLURE_PROPERTIES_PATH = "src/test/resources/allure.properties";
 
     public static Properties getDataOnFile() {
         Properties properties = new Properties();
@@ -18,10 +19,16 @@ public class ServiceData {
             exception.getStackTrace();
         }
         try (InputStreamReader reader = new InputStreamReader(
+                new FileInputStream(ALLURE_PROPERTIES_PATH))) {
+            properties.load(reader);
+        } catch (IOException exception) {
+            exception.getStackTrace();
+        }
+        try (InputStreamReader reader = new InputStreamReader(
                 new FileInputStream(TEST_DATA_PATH), StandardCharsets.UTF_8)) {
             properties.load(reader);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.getStackTrace();
         }
         return properties;
     }
